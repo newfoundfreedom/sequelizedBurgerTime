66745bf913432c9ae457ba221d95083671be0da5
Joel Roberts <newfoundfreedom@gmail.com>
2017-05-08 22:08:19 -0400

    prepping core files for sequelize

--- server.js
+++ server.js
@@ -4,31 +4,28 @@ const express = require("express"),
     // methodOverride = require("method-override");
 
 // app initialization
-const PORT = process.env.PORT || 4444;
+const PORT = process.env.PORT || 5555;
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+// Requiring our models for syncing
+const db = require("./models");
 
-// Configure Body-Parser
-app.use(bodyParser.urlencoded({ extended: false }));
+// Set Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Override with POST having ?_method=DELETE
-// app.use(methodOverride("_method"));
+// Static directory
+app.use(express.static("./public"));
 
-// Set Handlebars
-const exphbs = require("express-handlebars");
+// Routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-const routes = require("./controllers/burgertime_controller");
-
-app.use("/", routes);
-
-// Start server listening
-app.listen(PORT, function(){
-    console.log(`App listening on PORT ${PORT}`);
+// Sync sequelize models and then starting express app
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
-
